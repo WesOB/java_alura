e998e55d692bfa76de1ab5773ffb133b3685b277
Wes <wesx.si@gmail.com>
2021-09-07 16:49:21 -0300

    commit inicial

---
+++ sintaxe-variaveis-e-fluxo/src/TestaPontoFlutuante.java
@@ -0,0 +1,28 @@
+
+public class TestaPontoFlutuante {
+	public static void main(String[] args) {
+		double salario;
+		salario = 1250.70;
+		System.out.println("o salario é: " + salario);
+		
+		double divisao = 3.14/2;
+		
+		System.out.println(divisao);
+		
+//		int valor = 12.5;
+		
+//		int valor2 = 0.0;
+		
+		double teste = 125.50;
+//		int var = teste;
+		
+		int outraTentativa = 5/2;
+		
+		System.out.println(outraTentativa);
+		
+		double novaTentativa = 5.0/2;
+		
+		System.out.println(novaTentativa);
+		
+	}
+}
